a66c4883f068dd69377849128d2b5f8122557436
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-14 18:54:22 +0100

    script loading

--- src/app/azure-maps/azure-maps.component.ts
+++ src/app/azure-maps/azure-maps.component.ts
@@ -20,19 +20,19 @@ export class AzureMapsComponent implements OnInit {
   public loadScript() {
     console.log('preparing to load...')
 
-    // let style = document.createElement('link');
-    // style.rel = "stylesheet"
-    // style.href = "https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.css"
-    // style.type = "text/css"
-    // document.getElementsByTagName('head')[0].appendChild(style);
+    let style = document.createElement('link');
+    style.rel = "stylesheet"
+    style.href = "https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.css"
+    style.type = "text/css"
+    document.getElementsByTagName('head')[0].appendChild(style);
 
-    // let script1 = document.createElement('script');
-    // script1.src = "https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.js"
-    // document.getElementsByTagName('head')[0].appendChild(script1);
+    let script1 = document.createElement('script');
+    script1.src = "https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.js"
+    document.getElementsByTagName('head')[0].appendChild(script1);
 
-    // let script2 = document.createElement('script');
-    // script2.src = "https://atlas.microsoft.com/sdk/javascript/service/2/atlas-service.min.js"
-    // document.getElementsByTagName('head')[0].appendChild(script2);
+    let script2 = document.createElement('script');
+    script2.src = "https://atlas.microsoft.com/sdk/javascript/service/2/atlas-service.min.js"
+    document.getElementsByTagName('head')[0].appendChild(script2);
   }
 
   ngOnInit(): void {
