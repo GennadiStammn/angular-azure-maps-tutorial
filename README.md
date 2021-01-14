47d23e83ec78f58a496b30747a187d812a2adf09
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-14 19:57:39 +0100

    using azure-maps-control

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ https://docs.microsoft.com/de-de/azure/azure-maps/quick-demo-map-app
 https://github.com/Azure-Samples/AzureMapsCodeSamples/blob/master/AzureMapsCodeSamples/Tutorials/interactiveSearch.html
 
 https://docs.microsoft.com/de-de/azure/azure-maps/tutorial-search-location
+
+https://docs.microsoft.com/de-de/azure/azure-maps/how-to-use-map-control
--- src/app/azure-maps/azure-maps.component.ts
+++ src/app/azure-maps/azure-maps.component.ts
@@ -1,11 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
-
-declare namespace atlas {
-  class Map {
-      constructor(a?,b?)
-  }
-}
+import * as atlas from 'azure-maps-control';
 
 @Component({
   selector: 'app-azure-maps',
@@ -14,48 +9,19 @@ declare namespace atlas {
 })
 export class AzureMapsComponent implements OnInit {
 
-  loadAPI: Promise<any>;
+  constructor() { }
 
-  constructor() {
-    this.loadAPI = new Promise((resolve) => {
-      console.log('resolving promise...');
-      this.loadScript();
-    });
-  }
-
-
-  public loadScript() {
-    console.log('preparing to load...')
-
-    // let style = document.createElement('link');
-    // style.rel = "stylesheet"
-    // style.href = "https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.css"
-    // style.type = "text/css"
-    // document.getElementsByTagName('head')[0].appendChild(style);
-
-    // let script1 = document.createElement('script');
-    // script1.src = "https://atlas.microsoft.com/sdk/javascript/mapcontrol/2/atlas.min.js"
-    // document.getElementsByTagName('head')[0].appendChild(script1);
-
-    // let script2 = document.createElement('script');
-    // script2.src = "https://atlas.microsoft.com/sdk/javascript/service/2/atlas-service.min.js"
-    // document.getElementsByTagName('head')[0].appendChild(script2);
-  }
-
-  GetMap() {
-    //Add Map Control JavaScript code here.
-    //Instantiate a map object
+  createMap() {
     var map = new atlas.Map("myMap", {
-        //Add your Azure Maps subscription key to the map SDK. Get an Azure Maps key at https://azure.com/maps
         authOptions: {
-            authType: 'subscriptionKey',
-            subscriptionKey: '<Your Azure Maps Key>'
+            authType: atlas.AuthenticationType.subscriptionKey,
+            subscriptionKey: 'm4p6IZitk0R3wAH9abK4ZPmgphFrZLzCcRZoVMeEd0k'
         }
     });
 }
 
   ngOnInit(): void {
-    this.GetMap();
+    this.createMap();
   }
 
 }
