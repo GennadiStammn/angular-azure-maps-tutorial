1952b9426c6a3a6372ad4ccec50b4f4b617e6325
GennadiStammn <gennadi.stamm@googlemail.com>
2021-01-16 10:31:44 +0100

    search service

--- ng-azure-maps-tutorial/src/app/app.component.ts
+++ ng-azure-maps-tutorial/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as atlas from 'azure-maps-control';
+import { IMapEvent, SearchService } from 'ng-azure-maps';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,29 @@ import * as atlas from 'azure-maps-control';
 export class AppComponent {
   title = 'ng-azure-maps-tutorial';
 
+  constructor(private readonly searchService: SearchService) {
+  }
+
   mapClick(event: atlas.MapMouseEvent) {
     console.log(event.position)
+  }
+
+  mapReady(event: IMapEvent) {
+
+    this.searchService
+      .searchAddress("berlin", { countrySet: ['de', 'us'] })
+      .subscribe(response => {
+        const features = [];
+        for (const result of response.results) {
+          event.map.markers.add(new atlas.HtmlMarker({
+            position: [result.position.lon, result.position.lat]
+          }));
+          features.push(new atlas.data.Point([result.position.lon, result.position.lat]));
+        }
+
+        event.map.setCamera({
+          bounds: atlas.data.BoundingBox.fromData(features)
+        });
+      });;
   }
 }
